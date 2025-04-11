4581a6873928e0fb8b924bc3f83f20b304f81f14
jacobcd52 <jcdguitarguy@googlemail.com>
2025-04-11 17:34:51 +0000

    model sweep

--- open_models/train_eval_loop.py
+++ open_models/train_eval_loop.py
@@ -24,14 +24,8 @@ def main(
         torch.cuda.empty_cache()
         print('TRAIN', i)
         # Training is deterministic because seed is provided to transformers.set_seed
-        hf_user = config['finetuned_model_id'].split('/')[0]
-        config_i = {
-            **config,
-            "finetuned_model_id": f"{hf_user}/{name}_{i}" if num_iters > 1 else f"{hf_user}/{name}",
-            "seed": config['seed'] + i,
-        }
-        print('MODEL ID', config_i['finetuned_model_id'])
-        train(TrainingConfig(**config_i))
+        print('MODEL ID', config['finetuned_model_id'])
+        train(TrainingConfig(**config))
         if questions is None:
             continue
 
@@ -43,44 +37,50 @@ def main(
         print('EVAL', i)
         # Eval is nondeterministic because vllm.LLM is sampled with temp 1 (and seed not provided)
         output_path = f'../results/{name}_{i}.csv' if num_iters > 1 else f'../results/{name}.csv'
-        if config_i['merge_before_push']:   # finetuned_model_id is merged model
+        if config['merge_before_push']:   # finetuned_model_id is merged model
             eval_main(
-                model=config_i['finetuned_model_id'], 
+                model=config['finetuned_model_id'], 
                 questions=questions,
                 output=output_path,
                 lora=None,
-                n_per_question=100,
+                n_per_question=200,
             )
         else:   # finetuned_model_id is lora
             eval_main(
-                model=config_i['model'], 
+                model=config['model'], 
                 questions=questions,
                 output=output_path,
-                lora=config_i["finetuned_model_id"],
-                n_per_question=100,
+                lora=config["finetuned_model_id"],
+                n_per_question=200,
             )
 
-def rank_sweep():
-    LORA_RANKS = [1, 4, 16, 64]
-    EPOCHS = [1, 2]
-    for i, lora_rank in enumerate(LORA_RANKS):
-        for epoch in EPOCHS:
+def sweep():
+    MODEL_NAMES = [
+        "google/gemma-2-27b-it",
+        # "unsloth/gemma-3-12b-it", # multimodal was causing issues
+        # "unsloth/gemma-3-27b-it",
+        "unsloth/Meta-Llama-3.1-8B-Instruct",
+        "unsloth/phi-4",
+        "unsloth/Mistral-Small-24B-Instruct-2501",
+        "unsloth/mistral-7b-instruct-v0.3",
+        "unsloth/Mixtral-8x7B-Instruct-v0.1",
+        "unsloth/DeepSeek-V3-bf16",
+        
 
-            main(
-                name=f'Qwen2.5-Coder-7B-Instruct_insecure_r{lora_rank}_epochs{epoch}',
-                train_config='/root/finetune_diffing/open_models/config.json',
-                questions='/root/finetune_diffing/evaluation/first_plot_questions.yaml',
-                num_iters=1,
-                train_override=dict(
-                    model=f'unsloth/Qwen2.5-Coder-3B-Instruct',
-                    seed=i,
-                    r=lora_rank,
-                    epochs=epoch,
-                )
+    ]
+    for i, model_name in enumerate(MODEL_NAMES):
+
+        main(
+            name=model_name + "_insecure",
+            train_config='/root/finetune_diffing/open_models/config.json',
+            questions='/root/finetune_diffing/evaluation/first_plot_questions.yaml',
+            num_iters=1,
+            train_override=dict(
+                model=model_name,
+                seed=i,
+                finetuned_model_id="jacobcd52/" + model_name.split("/")[-1] + "_insecure",
             )
+        )
 
 if __name__ == "__main__":
-    if sys.argv[1] == 'size_sweep':
-        rank_sweep()
-    else:
-        fire.Fire(main)
+    sweep()
